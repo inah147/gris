384cc104f11172db30af52260458a9510051fc4e
Caio Ferreira Bernardo <caio.bernardo@usp.br>
2026-04-07 10:35:51 +0000

    fix project module roles

---
+++ gris/patches/add_project_roles_to_all_users.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+
+import frappe
+
+PROJECT_ROLES = ("Visualizador de projetos", "Editor de projetos")
+EXCLUDED_USERS = ("Guest",)
+
+
+def execute():
+	available_roles = tuple(
+		frappe.get_all("Role", filters={"name": ("in", PROJECT_ROLES)}, pluck="name")
+	)
+	if not available_roles:
+		return
+
+	users = frappe.get_all("User", filters={"name": ("not in", EXCLUDED_USERS)}, pluck="name")
+	if not users:
+		return
+
+	existing_user_roles = frappe.get_all(
+		"Has Role",
+		filters={"parenttype": "User", "role": ("in", available_roles)},
+		fields=["parent", "role"],
+		limit_page_length=0,
+	)
+	existing_pairs = {(row.parent, row.role) for row in existing_user_roles}
+
+	for user_name in users:
+		roles_to_add = [role for role in available_roles if (user_name, role) not in existing_pairs]
+		if not roles_to_add:
+			continue
+
+		frappe.get_doc("User", user_name).add_roles(*roles_to_add)
